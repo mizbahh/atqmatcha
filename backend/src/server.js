f65a780be424eb1cfdb9eb09daf5772e1b09148f
theshibao <shivam.dodol@gmail.com>
2026-04-03 13:25:41 -0400

    Add files via upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,27 +2,28 @@ import cors from "cors";
 import express from "express";
 import menuRoutes from "./routes/menuRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import authRoutes from "./routes/authRoutes.js"
+import authRoutes from "./routes/authRoutes.js";
 import eventRoutes from "./routes/eventRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import reviewRoutes from "./routes/reviewRoutes.js";
 import announcementRoutes from "./routes/announcementRoutes.js";
+import imageRoutes from "./routes/imageRoutes.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-
-
-const app = express()
+const app = express();
 const PORT = parseInt(process.env.PORT, 10) || 5001;
 
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+app.use(cors());
 
-//middleware to get access to the request body
+// middleware to get access to the request body
 app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.send("API is running");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/menuItems", menuRoutes);
@@ -31,14 +32,10 @@ app.use("/api/events", eventRoutes);
 app.use("/api/reviews", reviewRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/announcements", announcementRoutes);
+app.use("/api/images", imageRoutes);
 
-connectDB().then( () => {
-
-    app.listen(PORT, () =>{
-        console.log("Server started on PORT:", PORT);
-    });
-
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
-
-
-
